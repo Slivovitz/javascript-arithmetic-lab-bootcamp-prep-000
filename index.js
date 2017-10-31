dc3a09002e8c5990c909433696b031eade34f65c
Yosef Margolin <yukele@hotmail.com>
2017-10-31 00:43:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function makeInt(n) {
   var number = (n)
   return parseInt(number)
   return parseInt(number, 10)
-  return parseInt(hello)
+  return parseInt('hello')
 }
 
 function preserveDecimal(n) {
