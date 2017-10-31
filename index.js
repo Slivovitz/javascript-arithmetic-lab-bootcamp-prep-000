cd435809a42c8d1fc525e438d9fd3184c769417d
Yosef Margolin <yukele@hotmail.com>
2017-10-31 01:02:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function dec(n) {
 
 function makeInt(n) {
   var number = (n)
-  var string = (n)toString()
+  var string = toString(n)
   return parseInt(number)
   return parseInt(string, 10)
   return parseInt('hello')
